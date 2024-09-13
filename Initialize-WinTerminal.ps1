a7a94bfc7890c596695080f66d18b630c6e183c2
James Kehr <jakehr@microsoft.com>
2024-09-13 13:20:48 -0400

    v2.1

--- Initialize-WinTerminal.ps1
+++ Initialize-WinTerminal.ps1
@@ -35,97 +35,6 @@ if ($WSL.IsPresent) { Write-Verbose "WSL mode engaged." }
 ### FUNCTIONS ###
 #region
 
-# load the common functions
-. "$PSScriptRoot\lib\libFunc.ps1"
-
-<# FUNCTION: Find-GitRelease
-# PURPOSE:  Calls Github API to retrieve details about the latest release. Returns a PSCustomObject with repro, version (tag_name), and download URL.
-function Find-GitRelease {
-    [CmdletBinding()]
-    param (
-        [string]$Repo,
-        [switch]$Latest
-    )
-
-    Write-Verbose "Find-GitRelease - Begin"
-
-    # make sure we don't try to use an insecure SSL/TLS protocol when downloading files
-    $secureProtocols = @() 
-    $insecureProtocols = @( [System.Net.SecurityProtocolType]::SystemDefault, 
-                            [System.Net.SecurityProtocolType]::Ssl3, 
-                            [System.Net.SecurityProtocolType]::Tls, 
-                            [System.Net.SecurityProtocolType]::Tls11) 
-    foreach ($protocol in [System.Enum]::GetValues([System.Net.SecurityProtocolType])) { 
-        if ($insecureProtocols -notcontains $protocol) { 
-            $secureProtocols += $protocol 
-        } 
-    } 
-    [System.Net.ServicePointManager]::SecurityProtocol = $secureProtocols
-
-    if ($Latest.IsPresent) {
-        Write-Verbose "Find-GitRelease - Finding latest release."
-        $baseApiUri = "https://api.github.com/repos/$($repo)/releases/latest"
-    } else {
-        Write-Verbose "Find-GitRelease - Finding all releases."
-        $baseApiUri = "https://api.github.com/repos/$($repo)/releases"
-    }
-
-    # get the available releases
-    Write-Verbose "Find-GitRelease - Processing repro: $repo"
-    Write-Verbose "Find-GitRelease - Making Github API call to: $baseApiUri"
-    try {
-        if ($pshost.Version.Major -le 5) {
-            $rawReleases = Invoke-WebRequest $baseApiUri -UseBasicParsing -EA Stop
-        } elseif ($pshost.Version.Major -ge 6) {
-            $rawReleases = Invoke-WebRequest $baseApiUri -EA Stop
-        } else {
-            return (Write-Error "Unsupported version of PowerShell...?" -EA Stop)
-        }
-    } catch {
-        return (Write-Error "Could not get GitHub releases. Error: $_" -EA Stop)        
-    }
-
-    
-    if ($Latest.IsPresent) {
-        try {
-            [version]$version = ($rawReleases.Content | ConvertFrom-Json).tag_name
-        } catch {
-            $version = ($rawReleases.Content | ConvertFrom-Json).tag_name
-        }
-
-        $dlURI = ($rawReleases.Content | ConvertFrom-Json).Assets.browser_download_url
-
-        Write-Verbose "Find-GitRelease - Processing latest version."
-        $releases = [PSCustomObject]@{
-            Repo    = "$repo"
-            Version = "$version"
-            URL     = "$dlURI"
-        }
-
-        Write-Verbose "Find-GitRelease - Found: $version at $dlURI"
-    } else {
-        $releases = @()
-        Write-Verbose "Find-GitRelease - Processing $($version.Count) versions."
-        $jsonReleases = $rawReleases.Content | ConvertFrom-Json
-        for ($i = 0; $i -lt $jsonReleases.Count; $i++)
-        {
-            $tmpObj = [PSCustomObject]@{
-                Name    = $jsonReleases[$i].name
-                Version = [version]($jsonReleases[$i].tag_name.TrimStart("v"))
-                URL     = $jsonReleases[$i].Assets.browser_download_url
-            }
-
-            $releases += $tmpObj
-
-            Remove-Variable tmpObj -EA SilentlyContinue
-        }
-    }
-
-    Write-Verbose "Find-GitRelease - End"
-    return $releases
-} #end Find-GitRelease
-#>
-
 
 function Install-FromGithub {
     [CmdletBinding()]
@@ -248,11 +157,167 @@ function Install-Font {
     }
 }
 
+# Downloads a file from the Internet.
+# Returns the full path to the download.
+function Get-WebFile
+{
+    param ( 
+        [string]$URI,
+        [string]$Path,
+        [string]$FileName
+    )
+
+    Write-Debug "Get-WebFile - Start."
+
+    # validate path
+    if ( -NOT (Test-Path "$Path" -IsValid) ) {
+        return (Write-Error "The save path, $Path, is not valid. Error: $_" -EA Stop)
+    }
+
+    # create the path if missing
+    if ( -NOT (Get-Item "$Path" -EA SilentlyContinue) ) {
+        try {
+            $null = mkdir "$Path" -Force -EA Stop
+        } catch {
+            return (Write-Error "The save path, $Path, does not exist and cannot be created. Error: $_" -EA Stop)
+        }
+        
+    }
+
+    # create the full path
+    $OutFile = "$Path\$fileName"
+
+    # use curl if it is found in the path
+    # options are iwr (Invoke-WebRequest (default)), bits (Start-BitsTransfer), and curl (preferred when found)
+    $dlMethods = "iwr", "curl", "bits"
+    $dlMethod = "iwr"
+
+    # switch to curl when found
+    $curlFnd = Get-Command "curl.exe" -EA SilentlyContinue
+    if ($curlFnd) { $dlMethod = "curl" }
+
+    Write-Verbose "Get-WebFile - Attempting download of $URI to $OutFile"
+
+    # did the download work?
+    $dlWorked = $false
+
+    # methods tried
+    # initialize with curl because if curl is found then we're using it, if it's not found then we shouldn't try it
+    $tried = @("curl")
+
+    # loop through
+    do {
+        switch ($dlMethod) {
+            # tracks whether 
+            "curl" {
+                Write-Verbose "Get-WebFile - Download with curl."
+
+                Push-Location "$Path"
+                # download with curl
+                # -L = download location
+                # -o = output file
+                # -s = Silent
+                curl.exe -L $URI -o $OutFile -s
+                Pop-Location
+            }
+
+            "iwr" {
+                Write-Verbose "Get-WebFile - Download with Invoke-WebRequest."
+
+                # make sure we don't try to use an insecure SSL/TLS protocol when downloading files
+                Write-Debug "Get-WebFile - Disabling unsupported SSL/TLS protocls."
+                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12, [System.Net.SecurityProtocolType]::Tls13
+
+                # download silently with iwr
+                $oldProg = $global:ProgressPreference
+                $Global:ProgressPreference = "SilentlyContinue"
+                $null = Invoke-WebRequest -Uri $URI -OutFile "$OutFile" -MaximumRedirection 5 -PassThru
+                $Global:ProgressPreference = $oldProg
+            }
+
+            "bits" {
+                Write-Verbose "Get-WebFile - Download with Start-BitsTransfer."
+                
+                # download silently with iwr
+                $oldProg = $global:ProgressPreference
+                $Global:ProgressPreference = "SilentlyContinue"
+                $null = Start-BitsTransfer -Source $URI -Destination "$OutFile"
+                $Global:ProgressPreference = $oldProg
+            }
+
+            Default { return (Write-Error "An unknown download method was selected. This should not happen. dlMethod: $_" -EA Stop) }
+        }
+
+        # is there a file, any file, then consider this a success
+        $dlFnd = Get-Item "$OutFile" -EA SilentlyContinue
+
+        if ( -NOT $dlFnd ) {
+            # change download method and try again
+            Write-Verbose "Failed to download using $dlMethod."
+
+            if ($tried.Count -lt $dlMethods.Count) {
+                if ($dlMethod -notin $tried) {
+                    $tried += $dlMethod
+                    Write-Verbose "Get-WebFile - Added $dlMethod to tried: $($tried -join ', ')"
+                }
+
+                :dl foreach ($dl in $dlMethods) { 
+                    if ($dl -notin $tried) { 
+                        Write-Verbose "Get-WebFile - Switching to $dl method."
+                        $dlMethod = $dl
+                        $tried += $dl
+                        break dl
+                    }
+                }
+            } else {
+                return (Write-Error "The download has failed!" -EA Stop)
+            }
+        } else {
+            # exit the loop
+            $dlWorked = $true
+        }
+    } until ($dlWorked)
+
+    Write-Verbose "Get-WebFile - File downloaded to $OutFile."
+
+    #Add-Log "Downloaded successfully to: $output"
+    Write-Debug "Get-WebFile - Returning: $OutFile"
+    Write-Debug "Get-WebFile - End."
+    return $OutFile
+}
 
 #endregion FUNCTIONS
 
 ### CONSTANTS ###
 #region
+
+# make sure all the required files are present and try to download anything missing
+$fileList = Get-WebFile -URI 'https://raw.githubusercontent.com/JamesKehr/Initialize-WinTerminal/main/file.json' -Path "$PSScriptRoot" -FileName 'file.json'
+[array]$reqFiles = Get-Content $fileList | ConvertFrom-Json
+foreach ($rf in $reqFiles) {
+    # convert to hashtable
+    $htRF = $rf.PSObject.Properties | ForEach-Object -Begin {$h = @{}} -Process {$h."$($_.Name)" = $_.Value} -End {$h}
+
+    # replace . in the path with $PSScriptroot
+    $htRF.Path = $htRF.Path -replace "\.", "$PSScriptRoot"
+
+    if ( -NOT (Get-Item "$($htRF.Path)\$($htRF.FileName)" -EA SilentlyContinue) ) {
+        Write-Verbose "Downloading a missing file. htRF:`n$($htRF | Format-List | Out-String)`n"
+        
+        # download
+        $tryDL = $fileList = Get-WebFile @htRF
+        if (-NOT $tryDL) {
+            throw "Failed to download a required file."
+        }
+    } else {
+        Write-Verbose "File, $($htRF.Path)\$($htRF.FileName), was found."
+    }
+}
+
+
+
+# load the common functions
+. "$PSScriptRoot\lib\libFunc.ps1"
 
 # where to put downloads
 $savePath = "C:\Temp"
